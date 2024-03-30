e89232acf13de7959e1f73be850a3ad428f89e78
Abir Stolov <abir.stolov@vaica.com>
2024-03-30 18:07:10 +0300

    render tasks from var

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,29 @@
 import './Tasks.css'
 
 function Tasks() {
+    const tasks = [
+        {
+            id: 235,
+            desc: 'Task 1 description',
+            status: 'Blocked',
+            constrains: 'Task [12, 382]'
+        },
+        {
+            id: 432,
+            desc: 'Task 2 description',
+            status: 'Todo',
+            constrains: 'Time [8:00, 12:00]'
+        }
+    ]
+    let tasksElements = tasks.map(task => (
+        <div className="task">
+            <div className="id">{task.id}</div>
+            <div className="desc">{task.desc}</div>
+            <div className="status">{task.status}</div>
+            <div className="constrains">{task.constrains}</div>
+            <div className="menu">:</div>
+        </div>
+    ))
     return (
       <>
         <div className="tasks">
@@ -12,20 +35,7 @@ function Tasks() {
                     <button>Add</button>
                 </div>
             </div>
-            <div className="task">
-                <div className="id">235</div>
-                <div className="desc">Task 1 description</div>
-                <div className="status">Blocked</div>
-                <div className="constrains">Task [12, 382]</div>
-                <div className="menu">:</div>
-            </div>
-            <div className="task highlight">
-                <div className="id">432</div>
-                <div className="desc"> Task 2 description</div>
-                <div className="status">Todo</div>
-                <div className="constrains">Time [8:00, 12:00]</div>
-                <div className="menu">:</div>
-            </div>
+            {tasksElements}
         </div>
       </>
     )
