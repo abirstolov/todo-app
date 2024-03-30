71e42646de8a7afb5ed7bccb4e66dc391f8f1c76
Abir Stolov <abir.stolov@vaica.com>
2024-03-30 18:12:12 +0300

    render done-tasks from variable

--- src/components/DoneTasks.jsx
+++ src/components/DoneTasks.jsx
@@ -1,20 +1,29 @@
 
 function DoneTasks() {
+    const doneTasks = [
+        {
+            id: 34,
+            desc: 'Task 10 description',
+            constrains: 'Completed at 05/04/2024'
+        },
+        {
+            id: 21,
+            desc: 'Task 20 description',
+            constrains: 'Completed at 02/04/2024'
+        }
+    ]
+    let tasksElements = doneTasks.map(task => (
+        <div className="task">
+            <div className="id">{task.id}</div>
+            <div className="desc">{task.desc}</div>
+            <div className="constrains">{task.constrains}</div>
+            <div className="menu">:</div>
+        </div>
+    ))
     return (
       <>
         <div className="tasks">
-            <div className="task">
-                <div className="id">34</div>
-                <div className="desc">Task 10 description</div>
-                <div className="constrains">Completed at 05/04/2024</div>
-                <div className="menu">:</div>
-            </div>
-            <div className="task">
-                <div className="id">21</div>
-                <div className="desc">Task 20 description</div>
-                <div className="constrains">Completed at 02/04/2024</div>
-                <div className="menu">:</div>
-            </div>
+            {tasksElements}
         </div>
       </>
     )
